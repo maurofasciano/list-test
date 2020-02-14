3cc64f86d65477c29a865fd850e9bf802749ed10
840-G3-16-004 <m.fasciano@spediscitu.it>
2020-02-14 16:53:47 +0100

    Add project files.

---
+++ Liste_TEST/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Liste_TEST
+{
+    class Program
+    {
+        /// <summary>
+        /// Creazione istanza classe lista, altrimentio non passa
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var lista = new Liste();  
+            lista.List();
+        }
+    }
+
+    public class Liste
+    {
+        public void List()
+        {
+
+            int[] myInt = { 1, 15, 37, 9 };
+
+            myInt[0] = 100;
+
+            Console.WriteLine("Valore di posizione array {0}", myInt[0]);
+            Console.ReadLine();
+
+        }
+    }
+
+}
